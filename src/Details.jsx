894d7e01424e5eae9e68aca36ee324763413e32f
yfaheid <131709066+yfaheid@users.noreply.github.com>
2023-11-07 13:32:54 -0600

    Add Details form validation

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import imageIcon from "./assets/image.svg";
 import whiteImageIcon from "./assets/whiteimage.svg";
 import { useDetailsContext } from "./DetailsContext";
+import saveIcon from "./assets/save.svg";
 
 export default function Details() {
   const { userDetails, updateDetails, uploadedImage, updateUploadedImage } =
     useDetailsContext();
+
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
   const handleFirstNameChange = (e) => {
     updateDetails({ firstName: e.target.value });
@@ -34,6 +39,21 @@ export default function Details() {
 
   const handleUploadClick = () => {
     fileInput.current.click();
+  };
+
+  const handleSave = () => {
+    const hasFirstNameError = userDetails.firstName.trim() === "";
+    const hasLastNameError = userDetails.lastName.trim() === "";
+
+    setFirstNameError(hasFirstNameError);
+    setLastNameError(hasLastNameError);
+
+    if (!hasFirstNameError && !hasLastNameError) {
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    }
   };
 
   return (
@@ -92,24 +112,36 @@ export default function Details() {
                   <label className="text-xs grid gap-1" htmlFor="first-name">
                     First name*
                     <input
-                      className="border-light-gray border text-base pl-4 rounded-lg p-2.5"
+                      className={`border-light-gray border text-base pl-4 rounded-lg p-2.5 ${
+                        firstNameError ? "border-red" : ""
+                      }`}
                       id="first-name"
                       type="text"
                       value={userDetails.firstName}
                       onChange={handleFirstNameChange}
                       required
+                      placeholder="e.g. John"
                     />
+                    {firstNameError && (
+                      <p className="text-red text-xs">Can't be empty</p>
+                    )}
                   </label>
                   <label className="text-xs grid gap-1" htmlFor="last-name">
                     Last name*
                     <input
-                      className="border-light-gray border text-base pl-4 rounded-lg p-2.5"
+                      className={`border-light-gray border text-base pl-4 rounded-lg p-2.5 ${
+                        lastNameError ? "border-red" : ""
+                      }`}
                       id="last-name"
                       type="text"
                       value={userDetails.lastName}
                       onChange={handleLastNameChange}
                       required
+                      placeholder="e.g. Appleseed"
                     />
+                    {lastNameError && (
+                      <p className="text-red text-xs">Can't be empty</p>
+                    )}
                   </label>
                   <label className="text-xs grid gap-1" htmlFor="first-name">
                     Email
@@ -119,6 +151,7 @@ export default function Details() {
                       type="email"
                       value={userDetails.email}
                       onChange={handleEmailChange}
+                      placeholder="e.g. email@example.com"
                     />
                   </label>
                 </div>
@@ -128,10 +161,19 @@ export default function Details() {
         </div>
       </div>
       <div className="bg-white p-4 flex justify-center items-center rounded-bl-xl rounded-br-xl">
-        <button className="text-white w-full bg-dark-purple px-36 py-2.5 rounded-lg">
+        <button
+          onClick={handleSave}
+          className="text-white w-full bg-dark-purple px-36 py-2.5 rounded-lg"
+        >
           Save
         </button>
       </div>
+      {showSuccessMessage && (
+        <div className="w-90 flex gap-2 text-lighter-gray font-medium bg-dark-gray p-4 rounded-xl text-sm fixed bottom-24 left-1/2 transform -translate-x-1/2">
+          <img src={saveIcon} alt="save icon" />
+          Your changes have been successfully saved!
+        </div>
+      )}
     </div>
   );
 }
