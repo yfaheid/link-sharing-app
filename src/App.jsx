d38bdef946e911cbc3020e04b76123a9cdfdcb14
yfaheid <131709066+yfaheid@users.noreply.github.com>
2023-11-11 23:14:07 -0600

    Add min-height to App

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
             <Route
               path="/"
               element={
-                <div className="font-instrument h-screen bg-lighter-gray">
+                <div className="font-instrument min-h-screen bg-lighter-gray">
                   <Header />
                 </div>
               }
